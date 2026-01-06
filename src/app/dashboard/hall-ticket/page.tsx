42267849587b7ed8a9ab8113789f26c31cfadac4
Bingdev <bingdev505@gmail.com>
2026-01-06 17:16:09 +0000

    hall tiket page the  "Examination Hall Admit Card" inside make a scroll,

--- src/app/dashboard/hall-ticket/page.tsx
+++ src/app/dashboard/hall-ticket/page.tsx
@@ -56,7 +56,7 @@ export default async function HallTicketPage({ searchParams }: HallTicketPagePro
                 <CardTitle className="text-lg font-semibold">Examination Hall Admit Card</CardTitle>
             </CardHeader>
             <CardContent>
-                <div className="rounded-md border">
+                <div className="rounded-md border overflow-x-auto">
                 <Table>
                     <TableHeader>
                     <TableRow>
