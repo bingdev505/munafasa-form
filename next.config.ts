315bb7e843a8b5d217c54e6af3ca454264c378b7
Bingdev <bingdev505@gmail.com>
2026-01-06 19:54:33 +0000

    Error: Invalid src prop (https://res.cloudinary.com/dtgw2apjq/image/uplo

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,12 @@ const nextConfig: NextConfig = {
         hostname: 'drive.google.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
